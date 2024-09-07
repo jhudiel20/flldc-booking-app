0cc23e1c462a89b203a2af14dd27aa3923997316
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-07 18:48:01 +0800

    index

--- loader.js
+++ loader.js
@@ -19,38 +19,38 @@ function loadCSS(href) {
   
   // Load Fonts
   loadCSS('https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700&display=swap');
-  loadCSS(basePath + 'vendor/fonts/boxicons.css');
-  loadCSS(basePath + 'vendor/fonts/fontawesome.css');
-  loadCSS(basePath + 'vendor/fonts/flag-icons.css');
+//   loadCSS(basePath + 'vendor/fonts/boxicons.css');
+//   loadCSS(basePath + 'vendor/fonts/fontawesome.css');
+//   loadCSS(basePath + 'vendor/fonts/flag-icons.css');
   
-  // Load Core CSS
-  loadCSS(basePath + 'vendor/css/rtl/core.css');
-  loadCSS(basePath + 'vendor/css/rtl/theme-default.css');
-  loadCSS(basePath + 'css/demo.css');
+//   // Load Core CSS
+//   loadCSS(basePath + 'vendor/css/rtl/core.css');
+//   loadCSS(basePath + 'vendor/css/rtl/theme-default.css');
+//   loadCSS(basePath + 'css/demo.css');
   
-  // Load Vendors CSS
-  loadCSS(basePath + 'vendor/libs/perfect-scrollbar/perfect-scrollbar.css');
-  loadCSS(basePath + 'vendor/libs/typeahead-js/typeahead.css');
-  loadCSS(basePath + 'vendor/libs/swiper/swiper.css');
+//   // Load Vendors CSS
+//   loadCSS(basePath + 'vendor/libs/perfect-scrollbar/perfect-scrollbar.css');
+//   loadCSS(basePath + 'vendor/libs/typeahead-js/typeahead.css');
+//   loadCSS(basePath + 'vendor/libs/swiper/swiper.css');
   
-  // Load Page CSS
+//   // Load Page CSS
   loadCSS(basePath + 'vendor/css/pages/ui-carousel.css');
   
-  // Load Helpers and Config JS
-  loadJS(basePath + 'vendor/js/helpers.js');
-  loadJS(basePath + 'vendor/js/template-customizer.js');
-  loadJS(basePath + 'js/config.js');
+//   // Load Helpers and Config JS
+//   loadJS(basePath + 'vendor/js/helpers.js');
+//   loadJS(basePath + 'vendor/js/template-customizer.js');
+//   loadJS(basePath + 'js/config.js');
   
-  // Load Vendor JS
-  loadJS(basePath + 'vendor/libs/jquery/jquery.js');
-  loadJS(basePath + 'vendor/libs/popper/popper.js');
-  loadJS(basePath + 'vendor/js/bootstrap.js');
-  loadJS(basePath + 'vendor/libs/perfect-scrollbar/perfect-scrollbar.js');
-  loadJS(basePath + 'vendor/libs/hammer/hammer.js');
-  loadJS(basePath + 'vendor/libs/i18n/i18n.js');
-  loadJS(basePath + 'vendor/libs/typeahead-js/typeahead.js');
-  loadJS(basePath + 'vendor/js/menu.js');
-  loadJS(basePath + 'vendor/libs/swiper/swiper.js');
+//   // Load Vendor JS
+//   loadJS(basePath + 'vendor/libs/jquery/jquery.js');
+//   loadJS(basePath + 'vendor/libs/popper/popper.js');
+//   loadJS(basePath + 'vendor/js/bootstrap.js');
+//   loadJS(basePath + 'vendor/libs/perfect-scrollbar/perfect-scrollbar.js');
+//   loadJS(basePath + 'vendor/libs/hammer/hammer.js');
+//   loadJS(basePath + 'vendor/libs/i18n/i18n.js');
+//   loadJS(basePath + 'vendor/libs/typeahead-js/typeahead.js');
+//   loadJS(basePath + 'vendor/js/menu.js');
+//   loadJS(basePath + 'vendor/libs/swiper/swiper.js');
   
   // Load Main and Page-specific JS
   loadJS(basePath + 'js/main.js');
