d72b12ed387c72582fdd73fe4fce4e52800b2328
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-11 17:40:46 +0800

    index

--- js/sec-min.js
+++ js/sec-min.js
@@ -1,19 +1,19 @@
-// Get the current URL path
-const currentPath = window.location.pathname.split('/').pop() || 'index';
+ // Get the current URL path (without domain and query parameters)
+ const currentPath = window.location.pathname.split('/').pop() || 'index';
 
-// Get all nav-link elements
-const navLinks = document.querySelectorAll('.nav-link');
+ // Get all nav-link elements
+ const navLinks = document.querySelectorAll('.nav-link');
 
-// Iterate through each nav-link and compare its href with the current path
-navLinks.forEach(link => {
-  // Get the href of the link and remove leading '/'
-  const linkHref = link.getAttribute('href').split('/').pop();
-  
-  // If the href matches the current path, add the 'nav-link-active' class
-  if (linkHref === currentPath) {
-    link.classList.add('nav-link active');
-  }
-});
+ // Iterate through each nav-link and compare its href with the current path
+ navLinks.forEach(link => {
+   // Extract the relative path from the href attribute
+   const linkPath = link.getAttribute('href');
+
+   // If the href matches the current path, add the 'active' class
+   if (linkPath === currentPath) {
+     link.classList.add('active');
+   }
+ });
 
 
 // Function to load HTML files from the 'includes' folder into a specified element
