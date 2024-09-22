5331f4d6491cbff5afb10460c7493b312ed72ce2
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-22 22:49:55 +0800

    rating

--- public/js/sec-min.js
+++ public/js/sec-min.js
@@ -1,3 +1,68 @@
+
+document.querySelector('#ratingForm').addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent default form submission
+
+  // Show the loader
+  document.getElementById('loader').classList.add('show');
+
+  // Gather form data
+  const formData = {
+    rating: document.getElementById('rating').value
+    };
+
+
+  // Send form data to the backend via fetch
+  fetch('/api/ratings', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.errors) {
+      // If there are validation errors, display them
+      Swal.fire({
+        icon: 'error',
+        title: 'Validation Error',
+        html: data.errors.join('<br>'),  // Display each validation error on a new line
+      }).then(function() {
+        window.location.reload();
+    });
+    } else if (data.error) {
+      // If there is a backend error
+      Swal.fire({
+        icon: 'error',
+        title: 'Rating Failed',
+        text: data.error,
+      }).then(function() {
+        window.location.reload();
+    });
+    } else {
+      // On successful booking
+      Swal.fire({
+        icon: 'success',
+        title: 'Rating Submitted',
+      }).then(function() {
+        window.location.reload();
+    });
+    }
+  })
+  .catch(err => {
+    document.getElementById('loader').classList.remove('show');
+    // If there's a network error or other fetch issue
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: `An unexpected error occurred. Please try again. Error: ${err.message || err}`,
+    }).then(function() {
+      window.location.reload();
+  });
+  });
+});
+
+
 // Get the current URL path (without domain and query parameters)
  const currentPath = window.location.pathname.split('/').pop() || 'index';
 
