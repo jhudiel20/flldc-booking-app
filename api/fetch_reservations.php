5944aa552890467d6e028071894ed58a773f8417
jhudiel20 <sueltojhudiel20@gmail.com>
2024-09-28 20:44:56 +0800

    update

--- api/fetch_reservations.php
+++ api/fetch_reservations.php
@@ -1,6 +1,29 @@
 <?php
 // Include your database connection file
-require_once 'DBConnection.php';
+if(!defined('host')) define('host', $_ENV['POSTGRES_HOST']);
+if(!defined('username')) define('username', $_ENV['POSTGRES_USER']);
+if(!defined('password')) define('password', $_ENV['POSTGRES_PASSWORD']);
+if(!defined('db_tbl')) define('db_tbl', $_ENV['POSTGRES_DATABASE']);
+if(!defined('port')) define('port', 5432);
+
+Class DBConnection{
+    public $conn;
+    function __construct(){
+        try {
+            $this->conn = new PDO("pgsql:host=".host.";port=".port.";dbname=".db_tbl.";sslmode=require", username, password);
+            // Set error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database Connection Error: " . $e->getMessage());
+        }
+    }
+    function __destruct(){
+         $this->conn = null; // Close the connection
+    }
+
+}
+$db = new DBConnection();
+$conn = $db->conn;
 
 // Get room, time, and reserve_date from the request (via GET or POST)
 $room = $_POST['room'] ?? '';
