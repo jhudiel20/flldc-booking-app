497f0e119f56f9eb0c89b00cc09297e007ab4450
jhudiel20 <sueltojhudiel20@gmail.com>
2024-10-21 11:46:43 +0800

    update

--- api/available_rooms.js
+++ api/available_rooms.js
@@ -1,19 +1,14 @@
-const express = require('express');
-const router = express.Router();  // Use Express Router
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
-// Route to fetch room details
-router.get('/', async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM room_details');
-    res.json(result.rows);  // Send room data as JSON
+    res.status(200).json(result.rows);
   } catch (error) {
     console.error('Error fetching rooms:', error);
-    res.status(500).send('Server error');
+    res.status(500).json({ error: 'Server error' });
   }
-});
-
-module.exports = router;
+};
