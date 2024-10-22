8ec62c74e8b2db5bd86ae31de14650c06db14eaf
jhudiel20 <sueltojhudiel20@gmail.com>
2024-10-22 12:40:10 +0800

    update

--- api/fetch-image.js
+++ api/fetch-image.js
@@ -1,6 +1,6 @@
-const config = {
+const fetch_image = {
     githubOwner: process.env.GITHUB_OWNER,
     githubRepo: process.env.GITHUB_IMAGES,
 };
 
-export default config;
+export default fetch_image;
