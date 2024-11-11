a5e98d903b1492100b66fc6659ac7682570b16ef
jhudiel20 <sueltojhudiel20@gmail.com>
2024-11-11 20:32:56 +0800

    updates

--- api/rooms.js
+++ api/rooms.js
@@ -283,7 +283,7 @@ module.exports = async (req, res) => {
         // SQL query to insert data into the reservations table
         const result = await client.query(
           `INSERT INTO reservations (fname, lname, reserve_date, time, setup, business_unit, room, roomid, guest, contact, email, "table", chair, hdmi, extension, message, booking_id, date_created) 
-          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, NOW() AT TIME ZONE 'Asia/Manila')`,
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, NOW() AT TIME ZONE 'Asia/Manila')`,
           [fname, lname, reserve_date, time, setup, businessunit, roomName, roomID, guest, contact, email, table, chair, hdmi, extension, message, booking_id]
         );
 
