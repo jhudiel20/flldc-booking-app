77c5910aeb58259e3defdde1c9c4fb18e7280422
jhudiel20 <sueltojhudiel20@gmail.com>
2025-01-24 14:13:37 +0800

    update

---
+++ public/js/all-scripts.js
@@ -0,0 +1,26 @@
+// External Libraries
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+import AOS from "https://unpkg.com/aos@next/dist/aos.js";
+
+// Local Scripts
+import "./js/sec-min.js";
+import "./sneat/assets/vendor/libs/swiper/swiper.js";
+import "./sneat/assets/js/ui-carousel.js";
+
+// jQuery and Plugins
+import "./js/jquery-3.2.1.min.js";
+import "./js/jquery-migrate-3.0.0.js";
+import "./js/popper.min.js";
+import "./js/bootstrap.min.js";
+import "./js/owl.carousel.min.js";
+import "./js/jquery.waypoints.min.js";
+import "./js/jquery.stellar.min.js";
+import "./js/jquery.magnific-popup.min.js";
+import "./js/magnific-popup-options.js";
+
+// Vercel Analytics
+import { inject } from "https://cdn.jsdelivr.net/npm/@vercel/analytics/dist/index.mjs";
+inject();
+
+// Main Scripts
+import "./js/main.js";
