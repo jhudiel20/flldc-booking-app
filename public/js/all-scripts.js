2a63c16c59883753228eed5309e18975b8abda07
jhudiel20 <sueltojhudiel20@gmail.com>
2025-01-24 14:20:37 +0800

    update

--- public/js/all-scripts.js
+++ public/js/all-scripts.js
@@ -8,19 +8,19 @@ import "../sneat/assets/vendor/libs/swiper/swiper.js";
 import "../sneat/assets/js/ui-carousel.js";
 
 // jQuery and Plugins
-import "/public/js/jquery-3.2.1.min.js";
-import "./jquery-migrate-3.0.0.js";
-import "./js/popper.min.js";
-import "./js/bootstrap.min.js";
-import "./js/owl.carousel.min.js";
-import "./js/jquery.waypoints.min.js";
-import "./js/jquery.stellar.min.js";
-import "./js/jquery.magnific-popup.min.js";
-import "./js/magnific-popup-options.js";
+import "../js/jquery-3.2.1.min.js";
+import "../js/jquery-migrate-3.0.0.js";
+import "../js/popper.min.js";
+import "../js/bootstrap.min.js";
+import "../js/owl.carousel.min.js";
+import "../js/jquery.waypoints.min.js";
+import "../js/jquery.stellar.min.js";
+import "../js/jquery.magnific-popup.min.js";
+import "../js/magnific-popup-options.js";
 
 // Vercel Analytics
 import { inject } from "https://cdn.jsdelivr.net/npm/@vercel/analytics/dist/index.mjs";
 inject();
 
 // Main Scripts
-import "./js/main.js";
+import "../js/main.js";
