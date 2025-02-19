cef9954a065901ef23f4ec546edcaa0d66dcb8d1
jhudiel20 <sueltojhudiel20@gmail.com>
2025-02-19 21:38:44 +0800

    update

--- api/ForgotPassword.js
+++ api/ForgotPassword.js
@@ -211,7 +211,7 @@ async function handleChangePassword(req, res) {
     return res.status(400).json({ error: "Token has expired" });
   }
 
-  if (user.reset_token_expiry === NULL) {
+  if (user.reset_token_expiry === 'NULL') {
     return res.status(400).json({ error: "Token has already been used. Please request password request again!" });
   }
 
