20161c6030e6a950f832bc955726793da803902b
jhudiel20 <sueltojhudiel20@gmail.com>
2025-02-21 16:40:46 +0800

    update

--- public/js/sec-min.js
+++ public/js/sec-min.js
@@ -300,8 +300,7 @@ function includeHTML(file, elementID) {
                   <div id="forgotErrorMessage" class="text-danger mt-3"></div>
                   <div id="recaptcha-container-forgot" class="mt-3" style="display: flex; justify-content: center; align-items: center;"></div>
               `,
-              showDenyButton: true, // Use Deny button for Back
-              denyButtonText: 'Back',
+              showCancelButton: true,
               confirmButtonText: 'Reset Password',
               didOpen: () => {
                   grecaptcha.render('recaptcha-container-forgot', {
@@ -343,12 +342,7 @@ function includeHTML(file, elementID) {
                       return false;
                   }
               }
-          }).then((result) => {
-            if (result.isDenied) {
-                // Go back to Login Modal
-                showLoginModal();
-            }
-        });
+          });
         });
 
         document.getElementById('registerLink').addEventListener('click', function () {
